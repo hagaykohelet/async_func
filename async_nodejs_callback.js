79a26cd2fab58990d233a51dde21cef78626ba9b
hagay kohelet <hagay2506@gmail.com>
2025-12-14 22:16:19 +0200

    practice

--- async_nodejs_callback.js
+++ async_nodejs_callback.js
@@ -1,6 +1,9 @@
+import { count } from "console";
 import fs from "fs"
 import { loadavg } from "os"
+import path from "path"
 
+// q1
 // fs.readFile("note.txt","utf8",(err,data)=>{
 //     console.log(data)
 //     fs.stat("note.txt",(err,stat)=>{
@@ -13,21 +16,56 @@ import { loadavg } from "os"
 //     })
 // })
 
-
-// fs.readdir("data", (err, files) => {
-//     if (err) {
+// q2
+// fs.readdir("data",(err)=>{
+//     if(err){
 //         console.error(err)
 //     }
-//     else {
-//         fs.lstat("data", (err) => {
-//             if (err) {
+//     else{
+//         fs.lstat("data/info.txt",(err,stat)=>{
+//             if(!stat.isFile()){
 //                 console.error(err)
 //             }
-//             else {
-//                 let path = require("path")
-//                 let p = path.join("data",files)
-//                 console.log(p)
+//             else{
+//                 console.log(path.join("data","info.txt"))
 //             }
 //         })
 //     }
 // })
+
+
+// q3
+
+// function countdown(number, callback) {
+//     while(number >= 0){
+//         console.log(number);
+//         number -=1
+//     }
+//     setTimeout(() => {
+//         const str = "times up"
+//         callback(str)
+//     }, 1000);
+    
+// }
+
+
+// countdown(5, function (str) {
+//     console.log(str)
+// })
+
+
+fs.readFile("quotes.txt",("utf8"),(err,data) =>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        const line = data.split("\n")
+        const filetredLines = line.filter(line=> line.trim() !== "")
+        console.log(filetredLines);
+        const updatedContent = filetredLines.join("\n")
+        
+        console.log(updatedContent);
+        
+    }
+})
+
